202b4f78e7e65546d29c7dabb67d549e51f80349
BlameDeng <476961779@qq.com>
2018-10-17 00:51:52 +0800

    项目初始化

---
+++ app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+
+const api = async (ctx, next) => {
+    console.log(ctx.request);
+
+}
+
+router.post('/api', api);
+
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(8989);
+console.log('Koa开始监听8989端口');
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <form action="/api" method="post" @submit.prevent>
+            <input type="text" name="username">
+            <button type="submit">111</button>
+        </form>
+        <router-view />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    };
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-size: 14px;
+        line-height: 1.8em;
+    }
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
